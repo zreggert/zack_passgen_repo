e23625ea57266170263cfbba92cb6c7e6f94cdec
elgalloloco <zreggert@gmail.com>
2021-03-02 13:31:44 -0500

    created a function to produce arrays for each type of charactered that can be included in the password

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
+function getCharCodesArray(startNum, endNum) {
+  var array = [];
+  for (i = startNum; i <= endNum; i++){
+    array.push(i);
+  }
+  return array;
+}
+
+var uppercaseCharCodes = getCharCodesArray(65, 90);
+var lowercaseCharCodes = getCharCodesArray(97, 122);
+var numCharCodes = getCharCodesArray(48, 57);
+var symbolsCharCodes = getCharCodesArray(33, 47)
+.concat(
+  getCharCodesArray(58, 64)
+  .concat(
+    getCharCodesArray(91, 96)
+    .concat(
+      getCharCodesArray(123, 126)
+    )));
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var finalPass = '';
+  var acceptedChar = '';
+
   var passLength = prompt("Chose between 8 - 128 characters.");
   console.log(passLength);
+    if (passLength < 8 || passLength > 128) {
+      return "Please choose a number between 8 - 128";
+    } 
   var hasUppercase = confirm('Do you want uppercase letters?');
   console.log(hasUppercase);
+    if (true) {
+
+    }
   var hasLowercase = confirm('Do you want lowercase letters?');
   console.log(hasLowercase);
   var hasNum = confirm('Do you want numbers?');
@@ -35,31 +64,27 @@ generateBtn.addEventListener("click", writePassword);
 
 // length 8 - 128 characters
 //www.net-comber.com/charset.html
+
+
+//EXPERIMENTED WITH RANDOM GENERATING FUNCTIONS
 // lowercase
-function getRandLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+// function getRandLower() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
 
-// uppercase
-function getRandUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+// // uppercase
+// function getRandUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
 
-// numbers
-function getRandNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+// // numbers
+// function getRandNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
 
-//special characters
-function getRandSymbol() {
-  var symbols = '!@#$%^&*()-={}[]/,.+';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+// //special characters
+// function getRandSymbol() {
+//   var symbols = '!@#$%^&*()-={}[]/,.+';
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+// }
 
-
-
-
-console.log(getRandLower());
-console.log(getRandUpper());
-console.log(getRandNumber());
-console.log(getRandSymbol());
