13816ac664d86d56a94d65e3af2246c9f4723119
elgalloloco <zreggert@gmail.com>
2021-03-01 23:20:09 -0500

    created functions to generate random characters in each category as well as created a function to receive input from tthe user about how many characters they want in the password and what types they wanted to use.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var finalPass = '';
+  var passLength = prompt("Chose between 8 - 128 characters.");
+  console.log(passLength);
+  var hasUppercase = confirm('Do you want uppercase letters?');
+  console.log(hasUppercase);
+  var hasLowercase = confirm('Do you want lowercase letters?');
+  console.log(hasLowercase);
+  var hasNum = confirm('Do you want numbers?');
+  console.log(hasNum);
+  var hasSymbols = confirm('Do you want symbols?');
+  console.log(hasSymbols);
+
+
+  return finalPass;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +29,37 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// length 8 - 128 characters
+//www.net-comber.com/charset.html
+// lowercase
+function getRandLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// uppercase
+function getRandUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// numbers
+function getRandNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+//special characters
+function getRandSymbol() {
+  var symbols = '!@#$%^&*()-={}[]/,.+';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
+
+console.log(getRandLower());
+console.log(getRandUpper());
+console.log(getRandNumber());
+console.log(getRandSymbol());
