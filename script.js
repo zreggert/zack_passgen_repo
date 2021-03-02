c63a447c1705728255f73b3eda1517af10779d25
elgalloloco <zreggert@gmail.com>
2021-03-02 15:25:21 -0500

    added comments to explain how the application works

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This function creates an array of numbers that I will assign to variables that will represent the different types of characters used in the generated password. The numbers within the arrays are the Character Code number of each letter, number, and symbol used.
 function getCharCodesArray(startNum, endNum) {
   var array = [];
   for (i = startNum; i <= endNum; i++){
@@ -6,6 +7,7 @@ function getCharCodesArray(startNum, endNum) {
   return array;
 }
 
+// These are the variables that contain the value of an array creatd by the previous function.
 var uppercaseCharCodes = getCharCodesArray(65, 90);
 var lowercaseCharCodes = getCharCodesArray(97, 122);
 var numCharCodes = getCharCodesArray(48, 57);
@@ -23,6 +25,7 @@ var symbolsCharCodes = getCharCodesArray(33, 47)
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// This function initiates once the generator button is clicked and proceeds to prompt or ask for confirmation about how many characters are used in the password and which types are to be used. Upon confirmation the array being confirmed is added to acceptedChar variable which is an empty array.
 function generatePassword() {
   var acceptedChar = [];
   var passLength = prompt("Chose between 8 - 128 characters.");
@@ -46,9 +49,11 @@ function generatePassword() {
     acceptedChar = acceptedChar.concat(symbolsCharCodes);
   }
 
+  // This last variable is an empty array that is presented as the password. To create this array we use a for loop to loop through the acceptedChar array and randomly selects characters from the array equal to the passLength provided to us by the user.
   var finalPass = []
   for (i = 0; i <= passLength; i++){
     var randChar = acceptedChar[Math.floor(Math.random() * acceptedChar.length)]
+    // String.fromCharCode is a function that translates the CharCode number into a string of its designated character.
     finalPass.push(String.fromCharCode(randChar))
   }
   return finalPass.join('');
